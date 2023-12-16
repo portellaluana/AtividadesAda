1558dbd244cc88ae4ac6d3c542f4cdcb1260276b
portellaluana <luanaportella@gmail.com>
2023-12-15 21:18:35 -0300

    fix: projeto final POO

--- POO/projeto-final/script.js
+++ POO/projeto-final/script.js
@@ -1,6 +1,16 @@
+let level = document.querySelector(".level");
+let item = document.querySelectorAll(".item");
 let currentLevel = 1;
-let value = document.querySelector('.value')
-let acabou = document.querySelector('.acabou')
+let acabou = document.querySelector(".acabou");
+let totalGanho = 0;
+let porta1 = document.querySelector('.porta1')
+let porta2 = document.querySelector('.porta2')
+let porta3 = document.querySelector('.porta3')
+let porta4 = document.querySelector('.porta4')
+let porta5 = document.querySelector('.porta5')
+
+const btnAtualizar = document.querySelector(".btn-refresh");
+
 class levels {
   constructor(nivel, itens) {
     this.nivel = nivel;
@@ -17,19 +27,44 @@ function sortearPorta() {
   const itensDoNivelAtual = eval(`level${currentLevel}.itens`);
   const indiceSorteado = Math.floor(Math.random() * itensDoNivelAtual.length);
   const valorSorteado = itensDoNivelAtual[indiceSorteado];
- 
-  console.log('itensDoNivelAtual', itensDoNivelAtual)
 
-  let arraySemSorteado = itensDoNivelAtual.indexOf(indiceSorteado)
-  itensDoNivelAtual.splice(arraySemSorteado, 1)
 
-  value.innerHTML = valorSorteado
-  console.log(' currentLevel',  currentLevel)
+  let arraySemSorteado = itensDoNivelAtual.indexOf(indiceSorteado);
+  itensDoNivelAtual.splice(arraySemSorteado, 1);
+
   currentLevel++;
-  if(currentLevel === 6){
-    acabou.innerHTML = 'acabou tentativas'
+
+  level.innerHTML = `Level ${currentLevel}`;
+
+  totalGanho = totalGanho + valorSorteado;
+
+  if (currentLevel === 6) {
+    level.innerHTML = `Acabou`;
+    btnAtualizar.style.display = "flex";
   }
-  if(valorSorteado === 0){
-    acabou.innerHTML = 'perdeu tudo'
+  
+  if (valorSorteado === 0) {
+    level.innerHTML = `Perdeu tudo`;
+    acabou.innerHTML = `Perdeu tudo`;
+    btnAtualizar.style.display = "flex";
+  } else acabou.innerHTML = `Total a ganhar R$ ${totalGanho}`;
+
+  if(currentLevel === 2){
+    porta1.classList.add('hide')
+    porta2.classList.remove('hide')
+  }
+  if(currentLevel === 3){
+    porta2.classList.add('hide')
+    porta3.classList.remove('hide')
+  }
+  if(currentLevel === 4){
+    porta3.classList.add('hide')
+    porta4.classList.remove('hide')
+  }
+  if(currentLevel === 5){
+    porta4.classList.add('hide')
+    porta5.classList.remove('hide')
   }
 }
+
+
